c62075711954aa45235d807061ec2f30938815f9
ecenurarslan <ecenurarslan@users.noreply.github.com>
2020-10-13 23:57:55 +0300

    mock data generator added

--- qmatic_widget/js/mock-data-generator.js
+++ qmatic_widget/js/mock-data-generator.js
@@ -1,64 +1,54 @@
 class MockDataGenerator {
 
-    menuDataArray = [];
+    tabDataArray = [];
 
     constructor() {
 
         try {
-            this.menuDataArray = [];
+            this.tabDataArray = [];
 
-            var menuData01 = {
-                menuOrder: 0,
-                menuTitle: "Hamburger Menu",
-                menuImageUrl: "./assets/burger.png",
-                menuContents: ["250 gr Hamburger Eti", "Patates", "5'li Soğan Halkası"],
-                menuBackgroundColor : "rgb(226, 96, 21)",
-                menuCost : "29,90"
+            var tabData01 = {
+                tabOrder: 0,
+                tabBooth: "4",
+                tabBackgroundColor : "rgb(190, 157, 252)",
+                tabQNum : "65"
             }
 
 
-            var menuData02 = {
-                menuOrder: 0,
-                menuTitle: "Ton Balıklı Salata Menu",
-                menuImageUrl: "./assets/salad.png",
-                menuContents: ["Ton Balığı", "Akdeniz Yeşillikleri", "Kruton"],
-                menuBackgroundColor : "rgb(40, 103, 185)",
-                menuCost : "24,90"
+            var tabData02 = {
+                tabOrder: 0,
+                tabBooth: "6",
+                tabBackgroundColor : "rgb(57, 55, 187)",
+                tabQNum : "22"
             }
 
-            var menuData03 = {
-                menuOrder: 0,
-                menuTitle: "Döner Wrap Menu",
-                menuImageUrl: "./assets/doner.png",
-                menuContents: ["Wrap Et Döner", "Yeşillikler", "Patates"],
-                menuBackgroundColor : "rgb(240, 177, 12)",
-                menuCost : "39,90"
+            var tabData03 = {
+                tabOrder: 0,
+                tabBooth: "3",
+                tabBackgroundColor : "rgb(252, 168, 157)",
+                tabQNum : "14"
             }
 
-            var menuData04 = {
-                menuOrder: 0,
-                menuTitle: "Taco Menu",
-                menuImageUrl: "./assets/burger.png",
-                menuContents: ["Taco", "Patates", "Cola", "Yeşillikler", "Patates"],
-                menuBackgroundColor : "rgb(226, 96, 21)",
-                menuCost : "24,00"
+            var tabData04 = {
+                tabOrder: 0,
+                tabBooth: "7",
+                tabBackgroundColor : "rgb(150, 191, 207)",
+                tabQNum : "99"
             }
 
-            var menuData05 = {
-                menuOrder: 0,
-                menuTitle: "Beef Menu",
-                menuImageUrl: "./assets/burger.png",
-                menuContents: ["Ton Balığı", "Patates", "Cola"],
-                menuBackgroundColor : "rgb(240, 177, 12)",
-                menuCost : "48,90"
+            var tabData05 = {
+                tabOrder: 0,
+                tabBooth: "1",
+                tabBackgroundColor : "rgb(179, 173, 177)",
+                tabQNum : "102"
             }
 
 
-            this.menuDataArray.push(menuData01);
-            this.menuDataArray.push(menuData02);
-            this.menuDataArray.push(menuData03);
-            this.menuDataArray.push(menuData04);
-            this.menuDataArray.push(menuData05);
+            this.tabDataArray.push(tabData01);
+            this.tabDataArray.push(tabData02);
+            this.tabDataArray.push(tabData03);
+            this.tabDataArray.push(tabData04);
+            this.tabDataArray.push(tabData05);
 
 
         } catch (exception) {}
@@ -67,15 +57,15 @@ class MockDataGenerator {
 
 MockDataGenerator.prototype.constructor = MockDataGenerator;
 
-//returns a random number between 0 and (menuDataArray.length)
+//returns a random number between 0 and (tabDataArray.length)
 MockDataGenerator.prototype.getRandomInt = function () {
-    return Math.floor(Math.random() * (this.menuDataArray.length));
+    return Math.floor(Math.random() * (this.tabDataArray.length));
 };
 
 MockDataGenerator.prototype.fetchRandomObject = function () {
 
     var returnObject = 0;
-    returnObject = this.menuDataArray[this.getRandomInt()];
+    returnObject = this.tabDataArray[this.getRandomInt()];
     return returnObject;
 
 };
